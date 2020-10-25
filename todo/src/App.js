92bc1b2ff4547b90922572bae797407bd708801a
Supuni22 <supunimanamperi@gmail.com>
2020-10-25 12:26:33 +0530

    Created file structure and routes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
+import AddUser from './components/AddUser'
+import EditUser from './components/EditUser'
 
 function App() {
   return (
     <div className="App">
       <h1>To do app</h1>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home}/>
+          <Route path="/add" component={AddUser}/>
+          <Route path="/edit/:id" component={EditUser}/>
+        </Switch>
+      </Router>
+     
     </div>
   );
 }
